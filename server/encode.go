9816b63a6f551944c8dc4d96ba36b6faee6f315f
liuzh <liuzh@llvision.com>
2018-08-01 17:48:05 +0800

    Added response temple.

--- server/encode.go
+++ server/encode.go
@@ -1,17 +1,85 @@
 package server
 
+import (
+	"encoding/json"
+
+	"github.com/wst-libs/wst-sdk/utils"
+)
+
 func PutFileResponse() []byte {
-	return []byte("")
+	s := ResPutFile{
+		utils.ResponseCommon{
+			Version: utils.Version,
+			SeqNum:  1,
+			From:    "",
+			To:      "",
+			Type:    "",
+			Number:  "",
+			Uid:     "",
+		},
+		utils.ID{
+			Id: "",
+		},
+	}
+	out, _ := json.Marshal(s)
+	return out
 }
 
 func GetFileResponse() []byte {
-	return []byte("")
+	s := ResGetFile{
+		utils.ResponseCommon{
+			Version: utils.Version,
+			SeqNum:  1,
+			From:    "",
+			To:      "",
+			Type:    "",
+			Number:  "",
+			Uid:     "",
+		},
+		GetFileData{
+			Id:     "",
+			Name:   "",
+			Type:   "",
+			Path:   "",
+			Url:    "",
+			Size:   "",
+			Bucket: "",
+			Object: "",
+			Desc:   "",
+		},
+	}
+	out, _ := json.Marshal(s)
+	return out
 }
 
 func UpdateFileResponse() []byte {
-	return []byte("")
+	s := ResUpdateFile{
+		utils.ResponseCommon{
+			Version: utils.Version,
+			SeqNum:  1,
+			From:    "",
+			To:      "",
+			Type:    "",
+			Number:  "",
+			Uid:     "",
+		},
+	}
+	out, _ := json.Marshal(s)
+	return out
 }
 
 func DeleteFileResponse() []byte {
-	return []byte("")
+	s := ResDeleteFile{
+		utils.ResponseCommon{
+			Version: utils.Version,
+			SeqNum:  1,
+			From:    "",
+			To:      "",
+			Type:    "",
+			Number:  "",
+			Uid:     "",
+		},
+	}
+	out, _ := json.Marshal(s)
+	return out
 }
