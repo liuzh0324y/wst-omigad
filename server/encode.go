36651af720a70da0f6a578f20f86d4508560dc13
liuzh <liuzh@llvision.com>
2018-08-01 17:08:05 +0800

    Added response for put,delete,post and get.

---
+++ server/encode.go
@@ -0,0 +1,17 @@
+package server
+
+func PutFileResponse() []byte {
+	return []byte("")
+}
+
+func GetFileResponse() []byte {
+	return []byte("")
+}
+
+func UpdateFileResponse() []byte {
+	return []byte("")
+}
+
+func DeleteFileResponse() []byte {
+	return []byte("")
+}
