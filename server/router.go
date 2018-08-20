14492b197dda6bc20702d4a15746f8c9c7b183ac
liuzh <liuzh@llvision.com>
2018-08-20 12:14:29 +0800

    Update mapping router.

--- server/router.go
+++ server/router.go
@@ -7,6 +7,6 @@ import (
 )
 
 func init() {
-	beego.Router("/api/v1/storage/file", &Controller{}, "put:PutFile;get:GetFile;post:UpdateFile;delete:DeleteFile")
+	beego.Router("/api/v1/cloudstorage/file", &Controller{}, "put:PutFile;get:GetFile;post:UpdateFile;delete:DeleteFile")
 	log.Println("Initialize Router.")
 }
